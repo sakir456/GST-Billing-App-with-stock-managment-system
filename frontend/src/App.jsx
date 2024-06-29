47481114c10b790519c378a287da255304510487
sakir456 <shaikhsakir1015@gmail.com>
2024-06-29 23:37:06 +0530

    signup signin form

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import AuthForm from './components/AuthForm'
+
+function App() {
+ 
+
+  return (
+    <div className=' relative w-full h-full '>
+    <AuthForm />
+    </div>
+  )
+}
+
+export default App
