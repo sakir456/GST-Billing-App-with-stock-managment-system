c06ec8a746d6a69fde3331911cf242184ecf9b3b
sakir456 <shaikhsakir1015@gmail.com>
2025-02-23 21:55:08 +0530

    fix: update token expiration and cookie settings in generateToken function

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,13 +1,19 @@
 import jwt from 'jsonwebtoken'
 
 const generateTokenAndSetCookie = (userId, res) => {
-      const token = jwt.sign({userId}, process.env.JWT_SECRET)
+      const token = jwt.sign({userId}, process.env.JWT_SECRET, {
+        expiresIn: "60d", 
+      })
 
       res.cookie("jwt", token,{
-         httpOnly: true, 
+        
+        
+        httpOnly: true, 
        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", 
          secure: process.env.NODE_ENV === "production",
-         })
+         maxAge: 60 * 24 * 60 * 60 * 1000,
+ 
+    })
 }
 
 export default generateTokenAndSetCookie;
