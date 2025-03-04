e656a69858b9e5490187d4296171fe99b973be07
sakir456 <shaikhsakir1015@gmail.com>
2025-03-04 21:04:54 +0530

    fix: adjust layout by removing fixed width from DashboardMain component

--- frontend/src/components/Dashboard/DashboardMain.jsx
+++ frontend/src/components/Dashboard/DashboardMain.jsx
@@ -6,7 +6,7 @@ import DashboardButton from "./DashboardButton";
 const DashboardMain = () => {
   return (
     <div className="mt-8 w-full flex ">
-      <div className="flex w-3/4 flex-col">
+      <div className="flex  flex-col">
        <DashboardButton/>
     </div>
     
